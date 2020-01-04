f2764de2cd3d26923c59917da773cada5bd33f59
ramdeepakdanda <59307041+ramdeepakdanda@users.noreply.github.com>
2020-01-04 15:01:18 +0530

    Update AddTwoNumbers2.java

--- AddTwoNumbers2.java
+++ AddTwoNumbers2.java
@@ -3,15 +3,7 @@ public class AddTwoNumbers2 {
 
     public static void main(String[] args) {
         
-        int num1, num2, sum;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter First Number: ");
-        num1 = sc.nextInt();
-        
-        System.out.println("Enter Second Number: ");
-        num2 = sc.nextInt();
-        
-        sc.close();
+        int num1=10, num2=21, sum;
         sum = num1 + num2;
         System.out.println("Sum of these numbers: "+sum);
     }
